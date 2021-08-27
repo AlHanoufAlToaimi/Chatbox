3db90c638f7545c2f518b7cde7bd932139c9090c
AlHanouf AlToaimi <aaltoaimi@stcs.com.sa>
2021-08-27 20:12:33 +0300

    final

--- server.js
+++ server.js
@@ -8,8 +8,13 @@ let http = require("http").Server(app);
 // creating socket.io reference with help of http module 
 let io = require("socket.io")(http);
 
-// http://localhost:9090
-app.get("/c1",(req,res)=> {
+// http://localhost:9090/firstclient
+app.get("/firstclient",(req,res)=> {
+    res.sendFile(__dirname+"/index.html");
+})
+
+// http://localhost:9090/secondclient
+app.get("/secondclient",(req,res)=> {
     res.sendFile(__dirname+"/index.html");
 })
 
@@ -17,6 +22,7 @@ app.get("/c1",(req,res)=> {
 let obj=require("mongodb").MongoClient;
 let url="mongodb://localhost:27017";
 
+users = [];
 obj.connect(url,(err,client)=>
 { 
     if(!err){
@@ -25,34 +31,36 @@ obj.connect(url,(err,client)=>
 // This function execute when client send the request. 
 io.on("connection",(socket)=> {
     console.log("Client connected");
+
+   socket.on('setUsername', function(data){
+      console.log(data);
+
+      if(users.indexOf(data) > -1){
+         socket.emit('userExists', data + ' username is taken! Try another.');
+      } else {
+         users.push(data);
+         socket.emit('userSet', {username: data});
+      }
+   });
+
+   socket.on('msg', function(data){
+      //Send message to everyone
+      io.sockets.emit('newmsg', data);
    
-    // to get the message from a client 
-    socket.on("obj",({name,message})=> {
-    
-        console.log({name,message});
-
-       // socket.broadcast.emit("obj",({name,message}));
-
-        let db=client.db("mern"); //connect to mern database
-        let row = { username : name, text : message };                         
-        db.collection("Chatbox").insertOne(row,(err,result)=>{
-            if(!err){
-                console.log(result);
-            }else{
-                console.log(err); 
-            }
-   // client.close();
+      //save data in database
+      let db=client.db("mern"); //connect to mern database
+      db.collection("Chatbox").insertOne(data,(err,result)=>{
+       if(!err){
+           console.log(result);
+       }else{
+           console.log(err); 
+       }
+// client.close();
 })
 })
-})   
-
-}else{
-        console.log(err);
-    } 
 })
-
-// function appendMessages(message) {  
-// }
+}
+})
 
 // run the server on port number using http reference. 
 http.listen(9090,()=>console.log("Server running on port number 9090"))
